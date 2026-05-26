ce16fd0ccc1f3aa99c9df8dd6d3c532fe64f51a3
SyedShahidAhamed <syedshahidahamed786@gmail.com>
2026-05-26 14:32:16 +0530

    Added global CORS config

--- task-manager-backend/src/main/java/com/example/taskmanager/controller/TaskController.java
+++ task-manager-backend/src/main/java/com/example/taskmanager/controller/TaskController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,7 +20,6 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/tasks")
-@CrossOrigin(origins = "*")
 public class TaskController {
 
     private final TaskService taskService;
