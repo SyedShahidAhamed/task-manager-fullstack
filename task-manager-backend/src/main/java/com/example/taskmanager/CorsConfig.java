2264cf6a4bfeddff49b07791f4e7d1c71bd6827f
SyedShahidAhamed <syedshahidahamed786@gmail.com>
2026-05-26 14:47:24 +0530

    Fixed preflight CORS

--- task-manager-backend/src/main/java/com/example/taskmanager/CorsConfig.java
+++ task-manager-backend/src/main/java/com/example/taskmanager/CorsConfig.java
@@ -11,12 +11,14 @@ public class CorsConfig {
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
+
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
-                        .allowedMethods("*")
-                        .allowedHeaders("*");
+                        .allowedOriginPatterns("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(false);
             }
         };
     }
